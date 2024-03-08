38e558b92c1518e4da383d4bdcd5f2121d207111
mohanshyamchaudhary <97430097+mohanshyamchaudhary@users.noreply.github.com>
2024-03-08 07:44:30 -0800

    Add files via upload

---
+++ hello.java
@@ -0,0 +1,10 @@
+public class hello{
+    public static void main(String args[]){
+    System.out.println("hello");
+    int a = 10;
+    int b = 20;
+    int c = a+b;
+    System.out.println(c);
+    
+}
+}
