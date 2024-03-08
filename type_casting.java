ad41be12f5f46aaf718b9392d4c2601c407f2b64
mohanshyamchaudhary <97430097+mohanshyamchaudhary@users.noreply.github.com>
2024-03-08 08:28:22 -0800

    Add files via upload

---
+++ type_casting.java
@@ -0,0 +1,11 @@
+public class type_casting {
+    public static void main(String[] args) {
+        // int a=10;
+        // long b=a;
+        // System.out.println(b);
+        long z=12;
+        int x=z;
+        System.out.println(x);
+        
+    }
+}
