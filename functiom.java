3efca28a80826c56118fcae9c83ab1cc5f6b23d7
mohanshyamchaudhary <97430097+mohanshyamchaudhary@users.noreply.github.com>
2024-03-09 22:19:00 +0530

    Add files via upload

---
+++ functiom.java
@@ -0,0 +1,33 @@
+// public class functiom {
+//     public static void hii(){
+//         System.out.println("hello Mohan");
+//     }
+//     public static void main(String[] args) {
+//         hii();
+//     }
+// }
+import java.util.*;
+// public class functiom {
+//     public static void  sum(){
+//         Scanner sc=new Scanner(System.in);
+//         int a=sc.nextInt();
+//         int b=sc.nextInt();
+//         int s=a+b;
+//         System.out.println("the sum is " + s);
+
+//     }
+//     public static void main(String[] args) {
+        
+//         sum();
+//     }
+// }
+public class functiom {
+    public static void sum(int num1,int num2){
+        int s=num1+num2;
+        System.err.println(s);
+    }
+    public static void main(String[] args) {
+          
+        sum()
+    }
+}
